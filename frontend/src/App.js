be43f5ecf178369f69c5a1c8eaea160121044a6c
sof1516 <teshomesofoniyas152@gmail.com>
2024-08-21 19:40:58 -0400

    made the signup page load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Signup from "./components/Signup";
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
