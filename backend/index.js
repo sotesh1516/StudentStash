3f35a5592b8c53fcbef5d3cf28b864434e597338
sof1516 <teshomesofoniyas152@gmail.com>
2025-01-01 01:33:24 -0500

    started implementing the signup feature on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,9 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const app = express()
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+
+app.listen(3000);
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,17 +1,43 @@
-const express = require("express")
+const express = require("express");
 
+const route = express.Router();
 
-const route = express.Router()
+route.post("auth/signup", async (req, res) => {
+  try {
+    var incomingUser = req.body;
+    if (
+      incomingUser.name === undefined ||
+      incomingUser.username == undefined ||
+      incomingUser.email == undefined ||
+      incomingUser.password == undefined
+    ) {
+      //pass for now
+    }
+    if (incomingUser.username.length < 2) {
+      //notify the use that the username is not long enough
+    }
+    var emailRegEx = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    var regExResult = emailRegEx.test(incomingUser.email);
+    if (!regExResult) {
+      //notify the user that the email format is not correct
+    }
+    const newUser = {
+      name: incomingUser.name,
+      username: incomingUser.username,
+      email: incomingUser.email,
+      password: incomingUser.password,
+      //add default values for the rest of the properties in the schemaÍ
+    };
 
+    //check if the username is already taken -> tricky
+    //check if the user already exists in the database using its email
+    //register the user
+    //hash the password
+    //register the user
+    //return the necassary information
+  } catch (error) {}
+});
 
-route.post("/signup", (req, res) => {
+route.get("/signin", (req, res) => {});
 
-})
-
-route.get("/signin", (req,res) => {
-
-})
-
-route.post("/resetPassword", (req, res) => {
-
-})
+route.post("/resetPassword", (req, res) => {});
